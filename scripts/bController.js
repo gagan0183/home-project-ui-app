bc6e2c34cd0f98df04344b1afba3610076376138
gagan0183 <g.deepsingh1@gmail.com>
2017-06-19 11:45:00 +0530

    integration with spring

--- scripts/bController.js
+++ scripts/bController.js
@@ -3,13 +3,14 @@ var request = require('request');
 module.exports.postBook = function(req, res) {
     var data = req.body;
     console.log("in node : " + JSON.stringify(data));
-    // var response = request('POST', 'http://192.168.1.153:8080/book', {
-    //      json: data,
-    //      'headers': {
-    //         'Access-Control-Allow-Origin': 'http://192.168.1.153:8080'
-    //      }
-    // });
-    // console.log("response : " + response.getBody());
-    // res.send(response.getBody());
-    res.send({val : "this" });
+    var response = request({
+         method: 'POST',
+         url: 'http://192.168.1.153:8080/book',
+         json: data,
+         'headers': {
+            'Access-Control-Allow-Origin': 'http://192.168.1.153:8080'
+         }
+    }, function(err, response, body) {
+        res.send(JSON.stringify(body));
+    });
 };
