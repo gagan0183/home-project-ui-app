71f4883bcad8532c5309bbc8594aa4a63c82e370
gagan0183 <g.deepsingh1@gmail.com>
2017-06-21 01:35:58 +0530

    Added ui router capabilities

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('app', ['ngMaterial']);
+var app = angular.module('app', ['ngMaterial', 'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    console.log("in config");
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('home');
+
+    $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'home.html'   
+        })
+        .state('addbook', {
+            url: '/addbook',
+            templateUrl: 'addBook.html',
+            controller: 'BookController'
+        });
+});
